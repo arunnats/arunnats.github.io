3cc402a73c8481593fbbfc4bbf70d3372a318f07
arunnats <arun_b220019cs@nitc.ac.in>
2024-06-11 12:23:27 +0530

    Added flix

--- public/scripts/typed.js
+++ public/scripts/typed.js
@@ -1,12 +1,18 @@
 // typedAnimation.js
-document.addEventListener('DOMContentLoaded', function() {
-    var options = {
-      strings: ["web developer", "programmer", "FOSS enthusiast","fast learner","creator"],
-      typeSpeed: 55,
-      backSpeed: 30,
-      loop: true
-    };
-  
-    var typed = new Typed('#typed-output', options);
-  });
-  
+document.addEventListener("DOMContentLoaded", function () {
+	var options = {
+		strings: [
+			"web developer",
+			"programmer",
+			"FOSS enthusiast",
+			"fast learner",
+			"creator",
+			"writer",
+		],
+		typeSpeed: 55,
+		backSpeed: 30,
+		loop: true,
+	};
+
+	var typed = new Typed("#typed-output", options);
+});
